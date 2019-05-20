a9b426d98610216238846cb3231174c26e1fb74d
haloninjamonkey <haloninjamonkey@gmail.com>
2019-05-20 09:37:27 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace golfcard2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
